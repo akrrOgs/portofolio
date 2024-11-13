8587024e89b6eab3eb8e18256aa93530894654fb
akrrOgs <dlordrog@gmail.com>
2024-11-13 14:57:20 +0700

    Fix Nav and logo

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
   };
 
   return (
-    <section className="c-space my-28">
+    <section className="c-space my-28" id="contact">
       <div className="relative min-h-screen flex items-center justify-center flex-col">
         <img
           src="/assets/terminal.png"
